7824baaacc2ccf889685a717b6d2519c55e224b9
yousabu <shbanmhmdywsf8@gmail.com>
2022-04-20 02:18:04 +0200

    backend done

--- secgrp.tf
+++ secgrp.tf
@@ -1,90 +1,90 @@
 resource "aws_security_group" "vprofile-bean-elb-sg" {
-  name = "vprofile-bean-elb-sg"
+  name        = "vprofile-bean-elb-sg"
   description = "securty group for bean-elb"
-  vpc_id = module.vpc.vpc_id
+  vpc_id      = module.vpc.vpc_id
 
   egress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 80
-    protocol  = "tcp"
-    to_port   = 80
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 resource "aws_security_group" "vprofile-bastion-sec" {
-  name = "vprofile-bastion-sec"
+  name        = "vprofile-bastion-sec"
   description = "security group for bastion"
-  vpc_id = module.vpc.vpc_id
+  vpc_id      = module.vpc.vpc_id
 
   egress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 22
-    protocol  = "tcp"
-    to_port   = 22
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
     cidr_blocks = [var.MYIP]
   }
 
 }
 
 resource "aws_security_group" "vprofile-prod-sg" {
-  name = "vprofile-prod-sg"
+  name        = "vprofile-prod-sg"
   description = "security group for beanstalk instances"
-  vpc_id = module.vpc.vpc_id
+  vpc_id      = module.vpc.vpc_id
 
   egress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 22
-    protocol  = "tcp"
-    to_port   = 22
+    from_port       = 22
+    protocol        = "tcp"
+    to_port         = 22
     security_groups = [aws_security_group.vprofile-bastion-sec.id]
   }
 }
 
 resource "aws_security_group" "vprofile-backend-sg" {
-  name = "vprofile-backend-sg"
+  name        = "vprofile-backend-sg"
   description = "for backend sevices"
-  vpc_id = module.vpc.vpc_id
+  vpc_id      = module.vpc.vpc_id
 
   egress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
+    from_port       = 0
+    protocol        = "-1"
+    to_port         = 0
     security_groups = [aws_security_group.vprofile-prod-sg.id]
   }
 }
 
 resource "aws_security_group_rule" "sec_group_allow_itself" {
-  from_port         = 0
-  protocol          = "tcp"
-  security_group_id = aws_security_group.vprofile-backend-sg.id
+  from_port                = 0
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.vprofile-backend-sg.id
   source_security_group_id = aws_security_group.vprofile-backend-sg.id
-  to_port           = 65535
-  type              = "ingress"
+  to_port                  = 65535
+  type                     = "ingress"
 }
 
 
